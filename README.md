3d01a3acb71cffc9c06ad5a0010d0ea1dca7b677
root <root@DESKTOP-V04JJ76.localdomain>
2021-01-08 20:43:14 +0800

    冲冲冲

--- README.md
+++ README.md
@@ -1,29 +1,112 @@
 # knm
-鼠标键盘流量包取证
----
-## 使用方法：
 
-~~~
-usage:python(3) knm.py <cmd> [arg] [opts]
+> 鼠标键盘流量包分析取证  
+> author:[**FzWjScj**](https://github.com/FzWjScJ)、[**DizzyK**](https://github.com/Dizzy-K)
+
+---
+
+## 环境要求
+
+1. python库
+   1. matplotlib
+   2. tqdm
+2. tshark(作者还没研究明白pyshark，暂时只能先靠着tshark提取。别问，问就是DizzyK菜，没得洗呜呜呜)
+
+### 安装方法
+
+```bash
+pip install -r requestment.txt
+apt-get install tshark
+```
+
+## 使用方法
+
+```bash
+python knm.py <cmd> [arg] [opts]
         cmds:
-            keyboard (<datafile>) <outfile>
-            mouse (<datafile>) <outfile>
-            pca <pcadile>(<outfile>)    转换pca的USB流量为data(文件)
-            -v          version
-~~~
+            pca(p) <pcadile> (<outfile>)            转换pca的USB流量为data(文件)
+            keyboard(k) <datafile> (<outfile>)      识别提取出的data(文件)并还原data(文件)为键盘输入内容
+            mouse(m) <datafile> (<outfile>)         识别提取出的data(文件)并还原data(文件)为坐标并绘图
+            -v          version                     读取版本号及作者信息
+```
 
 ---
 
-## 参数解释：
+## 参数解释
 
-datafile：USB数据，可以通过pca获得
+datafile：USB数据，可以通过初始流量包获得  
+outfile：输出文件(可以不输入，不输入时默认为outpca、outmouse、outkeyboard)  
+keyboard(k)：解析键盘流量  
+mouse(m)：解析鼠标流量  
+pca(p)：将pcapng等USB流量包模式提取有用数据  
+-v：版本描述及作者信息  
 
-outfile：输出文件
+---
 
-keyboard（k）：解析键盘流量 
+## 功能讲述
 
-mouse（m）：解析鼠标流量
+### DONE
 
-pca：将pcapng等USB流量包模式提取有用数据
+- [x] 读取流量包信息
+- [x] 分析鼠标、键盘流量
+- [x] 代码重构
+- [x] 键盘流量直接输出内容
+- [x] 键盘流量扩容字典
+- [x] 鼠标流量作图
+  - 轨迹作图
+  - 左右键作图
+- [x] 更改pca的输出，使之能够更好的应用于mouse和keyboard
+- [x] 报错信息完善
+  - cmd wrong
+  - 流量包文件不存在
+  - 未指明输出文件(默认为outpca、outmouse、outkeyboard)
+  - keyboard、mouse输入文件格式错误
+- [x] 进度条
 
--v：版本描述
+### TODO
+
+- [ ] *自动识别流量类别*
+- [ ] 外设流量分析
+  - [ ] 触摸板
+  - [ ] 打击垫
+  - [ ] *手柄*
+- [ ] 不标准的鼠标键盘流量协议分析
+- [ ] pyshark代替tshark
+- [ ] And so on......
+
+---
+
+## 使用范例
+
+1. 提取USB流量信息
+
+   ```python
+   python knm.py p usb.pcap [output filename]
+   ```
+
+   在当前文件夹得到提取结果(若未填写[output filename], 则默认为outpca.txt)
+
+2. 提取键盘流量信息
+
+   ```python
+   python knm.py k [input filename] [output filename]
+   ```
+
+   其中, [input filename]为必填项(若未填写[output filename], 则默认为outkeyboard.txt)。  
+   在当前文件夹得到提取结果并将结果直接打印在终端上。
+
+3. 提取鼠标流量信息
+
+   ```python
+   python knm.py m [input filename] [output filename]
+   ```
+
+   其中, [input filename]为必填项(若未填写[output filename], 则默认为outmouse.txt)  
+   在当前文件夹得到提取结果并询问是否按照坐标点作图
+
+4. 作图  
+   直接输入序号(0\1\2\3)
+   0. 作出全部鼠标轨迹图
+   1. 作出鼠标左击轨迹图
+   2. 是作出鼠标右击轨迹图
+   3. 是将上述三幅图合成一幅输出(但精度较低,请酌情使用)
--- knm.py
+++ knm.py
@@ -1,221 +1,273 @@
 import sys,os
-normalKeys = {
-    "04":"a", "05":"b", "06":"c", "07":"d", "08":"e",
-    "09":"f", "0a":"g", "0b":"h", "0c":"i", "0d":"j",
-     "0e":"k", "0f":"l", "10":"m", "11":"n", "12":"o",
-      "13":"p", "14":"q", "15":"r", "16":"s", "17":"t",
-       "18":"u", "19":"v", "1a":"w", "1b":"x", "1c":"y",
-        "1d":"z","1e":"1", "1f":"2", "20":"3", "21":"4",
-         "22":"5", "23":"6","24":"7","25":"8","26":"9",
-         "27":"0","28":"<RET>","29":"<ESC>","2a":"<DEL>", "2b":"\t",
-         "2c":"<SPACE>","2d":"-","2e":"=","2f":"[","30":"]","31":"\\",
-         "32":"<NON>","33":";","34":"'","35":"<GA>","36":",","37":".",
-         "38":"/","39":"<CAP>","3a":"<F1>","3b":"<F2>", "3c":"<F3>","3d":"<F4>",
-         "3e":"<F5>","3f":"<F6>","40":"<F7>","41":"<F8>","42":"<F9>","43":"<F10>",
-         "44":"<F11>","45":"<F12>"}
-shiftKeys = {
-    "04":"A", "05":"B", "06":"C", "07":"D", "08":"E",
-     "09":"F", "0a":"G", "0b":"H", "0c":"I", "0d":"J",
-      "0e":"K", "0f":"L", "10":"M", "11":"N", "12":"O",
-       "13":"P", "14":"Q", "15":"R", "16":"S", "17":"T",
-        "18":"U", "19":"V", "1a":"W", "1b":"X", "1c":"Y",
-         "1d":"Z","1e":"!", "1f":"@", "20":"#", "21":"$",
-          "22":"%", "23":"^","24":"&","25":"*","26":"(","27":")",
-          "28":"<RET>","29":"<ESC>","2a":"<DEL>", "2b":"\t","2c":"<SPACE>",
-          "2d":"_","2e":"+","2f":"{","30":"}","31":"|","32":"<NON>","33":"\"",
-          "34":":","35":"<GA>","36":"<","37":">","38":"?","39":"<CAP>","3a":"<F1>",
-          "3b":"<F2>", "3c":"<F3>","3d":"<F4>","3e":"<F5>","3f":"<F6>","40":"<F7>",
-          "41":"<F8>","42":"<F9>","43":"<F10>","44":"<F11>","45":"<F12>"}
-VERSION = 0.1
+import matplotlib.pyplot as plot
+from tqdm import tqdm
 
-def start():
+normalKeys = {
+        "04":"a", "05":"b", "06":"c", "07":"d", "08":"e",
+        "09":"f", "0a":"g", "0b":"h", "0c":"i", "0d":"j",
+        "0e":"k", "0f":"l", "10":"m", "11":"n", "12":"o",
+        "13":"p", "14":"q", "15":"r", "16":"s", "17":"t",
+        "18":"u", "19":"v", "1a":"w", "1b":"x", "1c":"y",
+        "1d":"z","1e":"1", "1f":"2", "20":"3", "21":"4",
+        "22":"5", "23":"6","24":"7","25":"8","26":"9",
+        "27":"0", "28":"<RET>", "29":"<ESC>", "2a":"<DEL>", "2b":"<ALT>",
+        "2c":"<SPACE>","2d":"-","2e":"=","2f":"[","30":"]","31":"\\",
+        "32":"<NON>", "33":";","34":"'", "35":"<GA>", "36":",", "37":".",
+        "38":"/", "39":"<CAP>", "3a":"<F1>", "3b":"<F2>", "3c":"<F3>", "3d":"<F4>",
+        "3e":"<F5>", "3f":"<F6>", "40":"<F7>", "41":"<F8>", "42":"<F9>", "43":"<F10>",
+        "44":"<F11>", "45":"<F12>", "4a":"<HOME>", "4c":"<DELETE>", "4d":"<END>", "4f":"<RightArrow>", 
+        "50":"<LeftArrow>", "51":"<DownArrow>", "52": "<UpArrow>", "53":"<NumLock>", "54":"/", 
+        "55":"*", "56":"-", "57":"+", "58":"<RET>", "59":"1", "5a":"2", "5b":"3", "5c":"4", "5d":"5", 
+        "5e":"6", "5f":"7", "60":"8", "61":"9", "62":"0"}
+shiftKeys = {
+        "04":"A", "05":"B", "06":"C", "07":"D", "08":"E",
+        "09":"F", "0a":"G", "0b":"H", "0c":"I", "0d":"J",
+        "0e":"K", "0f":"L", "10":"M", "11":"N", "12":"O", 
+        "13":"P", "14":"Q", "15":"R", "16":"S", "17":"T",
+        "18":"U", "19":"V", "1a":"W", "1b":"X", "1c":"Y",
+        "1d":"Z","1e":"!", "1f":"@", "20":"#", "21":"$",
+        "22":"%", "23":"^","24":"&","25":"*","26":"(","27":")",
+        "28":"<RET>","29":"<ESC>","2a":"<DEL>", "2b":"<ALT>","2c":"<SPACE>",
+        "2d":"_","2e":"+","2f":"{","30":"}","31":"|","32":"<NON>","33":"\"",
+        "34":":","35":"<GA>","36":"<","37":">","38":"?","39":"<CAP>","3a":"<F1>",
+        "3b":"<F2>", "3c":"<F3>","3d":"<F4>","3e":"<F5>","3f":"<F6>","40":"<F7>",
+        "41":"<F8>","42":"<F9>","43":"<F10>","44":"<F11>","45":"<F12>", 
+        "4a":"<HOME>", "4c":"<DELETE>", "4d":"<END>", "4f":"<RightArrow>", 
+        "50":"<LeftArrow>", "51":"<DownArrow>", "52": "<UpArrow>", "53":"<NumLock>", "54":"/", 
+        "55":"*", "56":"-", "57":"+", "58":"<RET>", "59":"1", "5a":"2", "5b":"3", "5c":"4", "5d":"5", 
+        "5e":"6", "5f":"7", "60":"8", "61":"9", "62":"0"}
+VERSION = 1.1
+
+#获取指令
+def start() -> int:
     if '-h' in sys.argv or '--help' in sys.argv or len(sys.argv) < 2:
         print('''
         usage:python(3) knm.py <cmd> [arg] [opts]
         cmds:
-            keyboard (<datafile>) <outfile>
-            mouse (<datafile>) <outfile>
-            pca <pcadile>(<outfile>)    转换pca的USB流量为data(文件)
-            -v          version
+            pca <pcadile> (<outfile>)           转换pca的USB流量为data(文件)
+            keyboard <datafile> (<outfile>)     识别提取出的data(文件)并还原data(文件)为键盘输入内容
+            mouse <datafile> (<outfile>)        识别提取出的data(文件)并还原data(文件)为坐标并绘图
+            -v          version                 读取版本号及作者信息
         ''')
-        exit(1)
+        return 0
     cmd = sys.argv[1]
-    if cmd != 'keyboard' and cmd != 'k' and cmd != 'mouse' and cmd != 'm' and cmd != 'pca' and cmd != 'p' and cmd != '-v':
-        print("Wrong cmd")
-        exit(1)
-    if '-v' in sys.argv:
+    if '-v' == sys.argv[1]:
         print('version:'+str(VERSION))
+        print('Thanks to the auther DizzyK && FzWjScj')
+        print('''
+            If U find any problem, please take Ur issue into Github \
+                https://github.com/Dizzy-K/knm OR https://github.com/FzWjScJ/knm''')
+        return 0
+    elif 'pca' == sys.argv[1] or 'p' == sys.argv[1]:
+        return analyse_pca(cmd=cmd)
+    elif 'mouse' == sys.argv[1] or 'm' == sys.argv[1]:
+        return analyse_mouse(cmd=cmd)
+    elif 'keyboard' in sys.argv or 'k' in sys.argv:
+        return analyse_keyboard(cmd=cmd)
+    else:
+        return 1 #cmd wrong
+
+# 提取usb流量信息
+def analyse_pca(cmd) -> int:
+    if 'pca' == sys.argv[1]:
+        p = sys.argv.index('pca')
+    elif 'p' == sys.argv[1]:
+        p = sys.argv.index('p')
+    if len(sys.argv) <= p+1:
+        return 1 # cmd wrong
+    if 'pcap' not in sys.argv[p+1]:
+        return 4 # input wrong
+    pca = str(sys.argv[p+1])
+    if len(sys.argv) == p+2:
+        sys.argv.append('outpca.txt')
+        os.popen('tshark -r ' + pca + ' -T fields -e usb.capdata | sed \'/^\s*$/d\'> ' + sys.argv[p+2])
+        return 3 # output wrong
+    if len(sys.argv) == p+3:
+        os.popen('tshark -r ' + pca + ' -T fields -e usb.capdata | sed \'/^\s*$/d\'> ' + sys.argv[p+2])
+        print('done!')
         exit(1)
-    if 'pca' in sys.argv or 'p' in sys.argv:
-        if 'pca' in sys.argv:
-            p = sys.argv.index('pca')
-        elif 'p' in sys.argv:
-            p = sys.argv.index('p')
-        if len(sys.argv) <= p+1:
-            print('Missing pca file for pca')
-            exit(1)
-        pca = str(sys.argv[p+1])
-        if len(sys.argv) == p+3:
-            os.popen('tshark -r ' + pca + ' -T fields -e usb.capdata > ' + sys.argv[p+2])
-            exit(1)
-        # elif len(sys.argv) == p+2:
-        #     data = os.popen('tshark -r ' + pca + ' -T fields -e usb.capdata').read()
-        #     if 'keyboard' in sys.argv or 'k' in sys.argv:
-        #         if 'keyboard' in sys.argv:
-        #             p = sys.argv.index('keyboard')
-        #         elif 'k' in sys.argv:
-        #             p = sys.argv.index('k')
-        #         if len(sys.argv) <= p + 1:
-        #             print('Missing file for keyboard')
-        #             exit(1)
-        #         output = []
-        #         f2 = open(sys.argv[p + 2], 'a')
-        #         for line in data:
-        #             if ':' not in line:
-        #                 for i in range(0, len(line)-1, 2):
-        #                     if i + 2 != len(line):
-        #                         out += line[i] + line[i + 1] + ":"
-        #                     else:
-        #                         out += line[i] + line[i + 1]
-        #             else:
-        #                 out = line
-        #             try:
-        #                 if out[0] != '0' or (out[1] != '0' and out[1] != '2') or out[3] != '0' or out[4] != '0' or out[
-        #                     9] != '0' or out[10] != '0' or out[12] != '0' or out[13] != '0' or out[15] != '0' or out[
-        #                     16] != '0' or out[18] != '0' or out[19] != '0' or out[21] != '0' or out[22] != '0' or out[
-        #                                                                                                           6:8] == "00":
-        #                     continue
-        #                 if out[6:8] in normalKeys.keys():
-        #                     output += [[normalKeys[out[6:8]]], [shiftKeys[out[6:8]]]][out[1] == '2']
-        #                 else:
-        #                     output += ['[unknown]']
-        #             except:
-        #                 pass
-        #         for data in output:
-        #             f2.write(data)
-        #         f2.close()
-        #         exit(1)
+    else:
+        print('Unknown Wrong, please take it into issue!')
+        return 1 # other wrong
+
+# 键盘流量分析
+def analyse_keyboard(cmd) -> int:
+    if 'keyboard' in sys.argv:
+        p = sys.argv.index('keyboard')
+    elif 'k' in sys.argv:
+        p = sys.argv.index('k')
+    if len(sys.argv) < p + 2:
+        print('Missing file for keyboard')
+        return 2
+    elif len(sys.argv) == p + 2:
+        sys.argv.append('outkeyboard.txt')
+        print('Missing outputfile, it will named outkeyboard by default')
+    output = []
+    f1 = open(sys.argv[p+1],'r').readlines()
+    f2 = open(sys.argv[p+2],'w')
+    f1_ = tqdm(f1)
+    print("Taking keyboard data...")
+    for line in f1_:
+        if ':' not in line:
+            out = ''
+            for i in range(0, len(line)-1, 2):
+                if i + 2 != len(line):
+                    out += line[i] + line[i + 1] + ":"
+                else:
+                    out += line[i] + line[i + 1]
         else:
-            print('Missing pca file for pca')
-            exit(1)
-
-            if 'mouse' in sys.argv or 'm' in sys.argv:
-                if 'mouse' in sys.argv:
-                    p = sys.argv.index('mouse')
-                else:
-                    p = sys.argv.index('m')
-                if len(sys.argv) <= p + 1:
-                    print('Missing file for mouse')
-                    exit(1)
-                keys = data
-                f = open(sys.argv[p + 2], 'w')
-                posx = 0
-                posy = 0
-                for line in keys:
-                    if ':' not in line:
-                        out = ''
-                        for i in range(0, len(line)-1, 2):
-                            if i + 2 != len(line):
-                                out += line[i] + line[i + 1] + ":"
-                            else:
-                                out += line[i] + line[i + 1]
-                    else:
-                        out = line
-                    line = out[:12]
-                    x = int(line[3:5], 16)
-                    y = int(line[6:8], 16)
-                    if x > 127:
-                        x -= 256
-                    if y > 127:
-                        y -= 256
-                    posx += x
-                    posy += y
-                    btn_flag = int(line[0:2], 16)
-                    if btn_flag == 1:
-                        f.write(str(posx))
-                        f.write(' ')
-                        f.write(str(posy))
-                        f.write('\n')
-                f.close()
-                exit(1)
-
-
-    if 'keyboard' in sys.argv or 'k' in sys.argv:
-        if 'keyboard' in sys.argv:
-            p = sys.argv.index('keyboard')
-        elif 'k' in sys.argv:
-            p = sys.argv.index('k')
-        if len(sys.argv) <= p + 2:
-            print('Missing file for keyboard')
-            exit(1)
-        output = []
-        f1 = open(sys.argv[p+1],'r').readlines()
-        f2 = open(sys.argv[p+2],'a')
-        for line in f1:
-            if ':' not in line:
-                out = ''
-                for i in range(0, len(line)-1, 2):
-                    if i + 2 != len(line):
-                        out += line[i] + line[i + 1] + ":"
-                    else:
-                        out += line[i] + line[i + 1]
+            out = line
+        try:
+            if out[0] != '0' or (out[1] != '0' and out[1] != '2') or out[3] != '0' or out[4] != '0' or out[
+                9] != '0' or out[10] != '0' or out[12] != '0' or out[13] != '0' or out[15] != '0' or out[
+                16] != '0' or out[18] != '0' or out[19] != '0' or out[21] != '0' or out[22] != '0' or out[6:8] == "00":
+                continue
+            if out[6:8] in normalKeys.keys():
+                output += [[normalKeys[out[6:8]]], [shiftKeys[out[6:8]]]][out[1] == '2']
             else:
-                out = line
-            try:
-                if out[0] != '0' or (out[1] != '0' and out[1] != '2') or out[3] != '0' or out[4] != '0' or out[
-                    9] != '0' or out[10] != '0' or out[12] != '0' or out[13] != '0' or out[15] != '0' or out[
-                    16] != '0' or out[18] != '0' or out[19] != '0' or out[21] != '0' or out[22] != '0' or out[                                                                                      6:8] == "00":
-                    continue
-                if out[6:8] in normalKeys.keys():
-                    output += [[normalKeys[out[6:8]]], [shiftKeys[out[6:8]]]][out[1] == '2']
+                output += ['[unknown]']
+        except:
+            pass
+    print("Done!\nClearing data...")
+    for i in tqdm(range(len(output))):
+        try:
+            a=output.index('<DEL>')
+            del output[a]
+            del output[a-1]
+        except:
+            pass
+        try:
+            a=output.index('<RET>')
+            del output[a]
+            output.append('\n')
+        except:
+            pass
+        try:
+            a=output.index('<SPACE>')
+            del output[a]
+            output.append(' ')
+        except:
+            pass
+        try:
+            a=output.index('<ALT>')
+            del output[a]
+            output.append('\t')
+        except:
+            pass
+    flag = ''
+    for data in output:
+        f2.write(data)
+        flag += data
+    print("Data of keyboard pca is :\n", flag, end='')
+    f2.close()
+    exit(1)
+
+# 鼠标流量分析
+def analyse_mouse(cmd) -> int:
+    if 'mouse' in sys.argv:
+        p = sys.argv.index('mouse')
+    elif 'm' in sys.argv:
+        p = sys.argv.index('m')
+    if len(sys.argv) < p + 2:
+        print('Missing file for mouse')
+        return 2
+    if len(sys.argv) == p + 2:
+        sys.argv.append('outmouse.txt')
+        print('Missing outputfile, it will named outmouse by default')
+    name = sys.argv[p + 2].split('.')[0]
+    keys = open(sys.argv[p+1],'r').readlines()
+    f = open(sys.argv[p+2],'w')
+    posx = 0
+    posy = 0
+    print('Taking Data...')
+    for line in tqdm(keys):
+        if ':' not in line:
+            out = ''
+            for i in range(0, len(line)-1, 2):
+                if i + 2 != len(line):
+                    out += line[i] + line[i + 1] + ":"
                 else:
-                    output += ['[unknown]']
-            except:
-                pass
-        for data in output:
-            f2.write(data)
-        f2.close()
+                    out += line[i] + line[i + 1]
+        else:
+            out = line
+        if len(out) != 12:
+            continue
+        line = out[:12]
+        x = int(line[3:5], 16)
+        y = int(line[6:8], 16)
+        if x > 127:
+            x -= 256
+        if y > 127:
+            y -= 256
+        posx += x
+        posy += y
+        btn_flag = int(line[0:2], 16)
+        f.write(str(btn_flag))
+        f.write(' ')
+        f.write(str(posx))
+        f.write(' ')
+        f.write(str(posy))
+        f.write('\n')
+    f.close()
+    print('Done!')
+    change = input('Do U want to draw out?(Y/N)')
+    if change == 'Y' or change == 'y' or change == '':
+        draw_mouse(filename=name)
+    else:
         exit(1)
 
-    if 'mouse' in sys.argv or 'm' in sys.argv:
-        if 'mouse' in sys.argv:
-            p = sys.argv.index('mouse')
-        elif 'm' in sys.argv:
-            p = sys.argv.index('m')
-        if len(sys.argv) <= p + 2:
-            print('Missing file for mouse')
-            exit(1)
-        keys = open(sys.argv[p+1],'r').readlines()
-        f = open(sys.argv[p+2],'w')
-        posx = 0
-        posy = 0
-        for line in keys:
-            if ':' not in line:
-                out = ''
-                for i in range(0, len(line)-1, 2):
-                    if i + 2 != len(line):
-                        out += line[i] + line[i + 1] + ":"
-                    else:
-                        out += line[i] + line[i + 1]
-            else:
-                out = line
-            line = out[:12]
-            x = int(line[3:5], 16)
-            y = int(line[6:8], 16)
-            if x > 127:
-                x -= 256
-            if y > 127:
-                y -= 256
-            posx += x
-            posy += y
-            btn_flag = int(line[0:2], 16)
-            if btn_flag == 1:
-                f.write(str(posx))
-                f.write(' ')
-                f.write(str(posy))
-                f.write('\n')
-        f.close()
-        exit(1)
+# 鼠标坐标作图
+def draw_mouse(filename="outmouse"):
+    print('Drawing...')
+    a = input('Please input numof U want:\n0:All\n1:Only Left\n2:Only Right\n3:All of above\n')
+    if a == '0' or a == '1' or a == '2':
+        a = int(a)
+        fig = plot.figure()
+        ax = fig.add_subplot(111)
+        ax.set(title='QTMD', ylabel='nmb', xlabel='nmd')
+        print('Drawing...')
+        with open("outmouse.txt", 'r') as f:
+            for i in tqdm(f.readlines()):
+                btn_flag = int(i.split()[0])
+                x = int(i.split()[1])
+                y = int(i.split()[2])
+                if btn_flag == a:
+                    ax.plot(x, y, 'b.')
+
+    elif a == '3':
+        fig, axs = plot.subplots(2, 2)
+        #print(type(axs))
+        ax1 = axs[0, 0]
+        ax2 = axs[0, 1]
+        ax3 = axs[1, 0]
+        ax4 = axs[1, 1]
+        with open("outmouse.txt", 'r') as f:
+            for i in tqdm(f.readlines()):
+                btn_flag = int(i.split()[0])
+                x = int(i.split()[1])
+                y = int(i.split()[2])
+                if btn_flag == 0:
+                    ax1.plot(x, y, 'b.', label='All')
+                    ax1.set_title('All')
+                elif btn_flag == 1:
+                    ax2.plot(x, y, 'r.', label='Left')
+                    ax2.set_title('Left')
+                elif btn_flag == 2:
+                    ax3.plot(x, y, 'g.', label='Right')
+                    ax3.set_title('Right')
+    print('Saving...')              
+    plot.savefig('./'+filename+'.png')
+    print('Picture have save as '+filename+a+'.png')
+    print('Done!')    
+    plot.show()
 
 if __name__ == '__main__':
-    start()
+    error = start()
+    if error == 1:
+        print("Wrong cmd")
+    elif error == 2:
+        print('Missing input file')
+    elif error == 3:
+        print('Missing outputfile, it will named outpca by default')
+    elif error == 4:
+        print('Wrong input file')
